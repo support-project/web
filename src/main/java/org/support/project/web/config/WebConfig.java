c2352daeda15ab40c9f31c70b7eb70b66c64540a
Koda <koda.masaru3@gmail.com>
2016-03-24 19:17:26 +0900

    Analyticsのキー値をWeb共通部に設定

--- src/main/java/org/support/project/web/config/WebConfig.java
+++ src/main/java/org/support/project/web/config/WebConfig.java
@@ -1,8 +1,13 @@
 package org.support.project.web.config;
 
+/** 設定キー値を保持するクラス */
 public class WebConfig {
-	public static final String ROLE_ADMIN = "admin";
-	public static final String ROLE_USER = "user";
-	
+    /** ロール：管理者 */
+    public static final String ROLE_ADMIN = "admin";
+    /** ロール：ユーザ */
+    public static final String ROLE_USER = "user";
+
+    /** アナリティクス設定を保持するキー */
+    public static final String KEY_ANALYTICS_SCRIPT = "ANALYTICS_SCRIPT";
 
 }
