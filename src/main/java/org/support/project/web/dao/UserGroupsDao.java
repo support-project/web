f3e1b4a43e68e9e9c64ddd28ffde054987f65039
nagodon <nagodon@gmail.com>
2017-08-14 17:20:22 +0900

    Add UserGroupsDao.selectOnGroupIds

--- src/main/java/org/support/project/web/dao/UserGroupsDao.java
+++ src/main/java/org/support/project/web/dao/UserGroupsDao.java
@@ -1,9 +1,15 @@
 package org.support.project.web.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.support.project.common.util.StringJoinBuilder;
 import org.support.project.di.Container;
 import org.support.project.di.DI;
 import org.support.project.di.Instance;
+import org.support.project.ormapping.common.SQLManager;
 import org.support.project.web.dao.gen.GenUserGroupsDao;
+import org.support.project.web.entity.UserGroupsEntity;
 
 /**
  * ユーザが所属するグループ
@@ -23,4 +29,24 @@ public class UserGroupsDao extends GenUserGroupsDao {
         return Container.getComp(UserGroupsDao.class);
     }
 
+    /**
+     * グループのID配列からエンティティを取得する
+     * 
+     * @param Integer[] groupIds
+     * @return list
+     */
+    public List<UserGroupsEntity> selectOnGroupIds(List<Integer> groupIds) {
+        String sql = SQLManager.getInstance().getSql("/org/support/project/web/dao/sql/UserGroupsDao/UserGroupsDao_select_on_group_ids.sql");
+        StringJoinBuilder builder = new StringJoinBuilder();
+        List<Integer> params = new ArrayList<Integer>();
+
+        for (Integer groupId : groupIds) {
+            builder.append("?");
+            params.add(groupId);
+        }
+
+        sql = sql.replace("%GROUPS%", builder.join(", "));
+
+        return executeQueryList(sql, UserGroupsEntity.class, params.toArray(new Integer[0]));
+    }
 }
